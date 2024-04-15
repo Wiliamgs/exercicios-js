907d38470dafc0f4480f34e802a35912c9f1bd69
Wiliam Cruz <william.gabriel9132@gmail.com>
2024-04-15 18:08:11 -0300

    Primeiro Commit

---
+++ exercicio-01/consts.js
@@ -0,0 +1,10 @@
+let CurrentYear = 2015
+let Date = 19
+
+console.log("CurrentYear, Date")
+
+Year = 2022
+console.log("Year")
+
+const Points = 1000
+console.log("points")
---
+++ exercicio-01/strings.js
@@ -0,0 +1,11 @@
+console.log("Aula de Strings")
+
+const email = 'cruz.wiliam@grupotorun.com'
+console.log(email)
+
+const FirstName = "Wiliam"
+const LastName = "Cruz"
+
+const fullName = FirstName + " " + LastName
+console.log(fullName)
+
